4e2032ba0bc588ed4ddf2cdfb2a52383d5b119f4
major major <majorx234@gmail.com>
2017-03-03 06:49:58 +0100

    changed upstart syntax to systemd syntax, fixed minor flaws

---
+++ README.md
@@ -0,0 +1,2 @@
+# distributed_systemd_start
+This package provides a systemd autostart service to start ROS in a distributed environment. An additional config script is supplied which will help you to install the service.
---
+++ scripts/config.py
@@ -0,0 +1,251 @@
+#!/usr/bin/env python
+
+# This program requires super-user privileges due to writing
+# to /etc/init.
+#
+# DESIGN:
+#
+# -- systemd service files in /etc/systemd/system/
+#   -- starts zeroconf starter
+#
+# -- env_script in /home/{USER}/.ros/env/
+#   -- loads ROS environment
+#
+# -- zeroconf starter_script in pkg dir
+#   -- sources env script
+#   -- starts either zeroconf master or slave
+
+import os
+import stat
+import sys
+import socket
+import argparse
+import getpass
+import errno
+import subprocess
+
+import rospkg
+from catkin.find_in_workspaces import get_workspaces
+
+# env
+ROS_DISTRO = os.environ['ROS_DISTRO']
+ROS_BIN = '/opt/ros/{}/bin'.format(ROS_DISTRO)
+
+# this is just a global setting now, maybe its configurable later...
+ZEROCONF_SUFFIX = '.local'
+
+
+def check_settings():
+    global is_master
+    global user
+    global iface
+    global ws_path
+    global master_hostname
+    global hostname
+    print 'Settings:'
+    print '  Master:', is_master
+    print '  User:', user
+    print '  Master Hostname:', master_hostname
+    print '  Client Hostname:', hostname
+    print '  Interface:', iface
+    print '  Workspace:', ws_path
+    correct = None
+    while type(correct) is not bool:
+        correct = raw_input('Are these settings correct (y/n): ')
+        if correct == 'y' or correct == 'n':
+            correct = True if correct is 'y' else False
+        else:
+            print 'ERROR: You can only answer with "y" or "n".'.format(len(workspaces))
+    return correct
+
+
+def create_starter_script(user, is_master):
+    """
+    Create starter bash script.
+    """
+    template = [
+        '#!/bin/bash',
+        '',
+        'source /home/{}/.ros/env/distributed_ros.bash'.format(user),
+        'rosrun distributed_systemd_start distributed_ros_{}'.format('master' if is_master else 'slave'),
+    ]
+    return '\n'.join(template)
+
+
+def create_systemd_script(iface):
+    """
+    Create systemd service file.
+    """
+    global ROS_BIN
+    global user
+    template = [
+        '[Unit]'
+        'Description=Ros Distributed Systemd Start',
+        'After=sshd.service',
+        '[Service]',
+        'User={}'.format(user),
+        'Environment=ROSLAUNCH_SSH_UNKNOWN=1',
+        'ExecStart={}/distributed_ros'.format(ROS_BIN),
+        'Restart=on-abort',
+        '[Install]',
+        'WantedBy=default.target'
+    ]
+    return '\n'.join(template)
+
+
+def create_env_setup_script(ws_path, master_hostname, hostname):
+    """
+    Create ROS environment setup script.
+    """
+    template = [
+        '#!/bin/bash',
+        '',
+        'source {}/setup.bash'.format(ws_path),
+        '',
+        'export ROS_MASTER_URI="http://{}.local:11311"'.format(master_hostname),
+        'export ROS_HOSTNAME="{}.local"'.format(hostname),
+        '',
+        'export ROSLAUNCH_SSH_UNKNOWN=1',
+        '',
+        'exec "$@"',
+    ]
+    return '\n'.join(template)
+
+
+# args
+parser = argparse.ArgumentParser()
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument('-m', '--master', action="store_true", help='Configure this machine as a distributed ROS master.')
+group.add_argument('-s', '--slave', action="store_true", help='Configure this machine as a distributed ROS slave.')
+group.add_argument('-c', '--clean', action="store_true", help='Remove all startup/configuration files.')
+parser.add_argument('-i', '--interface', help='Configure network device on which service will wait till it starts.')
+args = parser.parse_args()
+
+# get username
+user = getpass.getuser()
+
+# get hostname
+hostname = socket.gethostname()
+
+# get package path
+rospack = rospkg.RosPack()
+pkg_path = rospack.get_path('distributed_systemd_start')
+
+# check clean
+if args.clean:
+    if os.path.exists('/home/{}/.ros/env/distributed_ros.bash'.format(user)):
+        os.remove('/home/{}/.ros/env/distributed_ros.bash'.format(user))
+        print 'Removed environment setup script.'
+    if os.path.exists('{}/distributed_ros'.format(ROS_BIN)):
+        os.remove('{}/distributed_ros'.format(ROS_BIN))
+        print 'Removed starter script.'
+    if os.path.exists('/etc/init/distributed-ros.conf'):
+        command = "sudo rm /etc/systemd/system/distributed-ros.service"
+        subprocess.call(["/usr/bin/sudo", "sh", "-c", command])
+        print 'Removed systemd service file.'
+    print "Done."
+    sys.exit(0)
+
+# check master
+is_master = args.master
+print 'Configuring {} as {}.'.format(hostname, 'MASTER' if is_master else 'SLAVE')
+if not is_master:
+    # build ROS_MASTER_URI from master hostname as zeroconf address
+    master_hostname = raw_input('ROS master hostname: ')
+    if master_hostname.endswith(ZEROCONF_SUFFIX):
+        print 'WARNING: zeroconf suffix "{}" is auto-appended.'.format(ZEROCONF_SUFFIX)
+        master_hostname = master_hostname[0:-len(ZEROCONF_SUFFIX)]
+else:
+    master_hostname = str(hostname)
+
+# check interface
+if args.interface:
+    iface = args.interface
+    try:
+        import netifaces
+        ifaces = netifaces.interfaces()
+        if iface not in ifaces:
+            print 'WARNING: Given interface ({}) not found in current interfaces ({}).'.format(iface, ', '.join(ifaces))
+    except:
+        print 'WARNING: Could not check if interface exists. Install python-netifaces to fix this problem.'
+else:
+    print 'Interface not provided (option -i INTERFACE): Set to default "eth0".'
+    iface = 'eth0'
+
+# get workspace path
+workspaces = get_workspaces()
+if len(workspaces) > 1:
+    print 'More than one workspace found on system. Select desired one by number:'
+    for i in range(len(workspaces)):
+        print '{}. {}'.format(i+1, workspaces[i])
+    number = 0
+    repeat = True
+    while repeat:
+        failed = False
+        try:
+            number = int(raw_input('Workspace nr.: '))
+            if 0 <= number-1 < len(workspaces):
+                repeat = False
+            else: failed = True
+        except:
+            failed = True
+        if failed: print 'ERROR: Number should be an integer between: 1-{}'.format(len(workspaces))
+    ws_path = workspaces[number-1]
+else:
+    ws_path = workspaces[0]
+
+# check settings before write
+print 'Verify settings (if settings are correct, files are written to their designated places):'
+if check_settings():
+
+    # write systemd service file (utilizing sudo)
+    systemd_script = create_systemd_script(iface)
+    try:
+        command = "echo '{}' > /etc/systemd/system/distributed-ros.service".format(systemd_script)
+        subprocess.call(["/usr/bin/sudo", "sh", "-c", command])
+    except:
+        raise
+    print 'Wrote systemd script to "/etc/systemd/system/distributed-ros.service".'
+    
+    #set file attributes
+    try:
+        command = "chmod +x /etc/systemd/system/distributed-ros.service"
+        subprocess.call(["/usr/bin/sudo", "sh", "-c", command])
+    except:
+        raise
+    print 'Changed attributes of systemd script "/etc/systemd/system/distributed-ros.service".'
+    
+    #enable systemd service
+    try:
+        command = "systemctl enable distributed-ros.service"
+        subprocess.call(["/usr/bin/sudo", "sh", "-c", command])
+    except:
+        raise
+    print ' enable Systemd Service "distributed-ros.service".'
+    
+    # write starter script (utilizing sudo)
+    starter_script = create_starter_script(user, is_master)
+    try:
+        command = "echo '{}' > {}/distributed_ros".format(starter_script, ROS_BIN)
+        subprocess.call(["/usr/bin/sudo", "sh", "-c", command])
+        command = "chmod +x {}/distributed_ros".format(ROS_BIN)
+        subprocess.call(["/usr/bin/sudo", "sh", "-c", command])
+    except:
+        raise
+    print 'Wrote starter script to "{}/distributed_ros".'.format(ROS_BIN)
+
+
+    # write environment script
+    env_setup_script = create_env_setup_script(ws_path, master_hostname, hostname)
+    if not os.path.exists('/home/{}/.ros/env'.format(user)):
+        os.mkdir('/home/{}/.ros/env'.format(user))
+    with open('/home/{}/.ros/env/distributed_ros.bash'.format(user), 'w+') as f:
+        f.write(env_setup_script)
+    st = os.stat('/home/{}/.ros/env/distributed_ros.bash'.format(user))
+    os.chmod('/home/{}/.ros/env/distributed_ros.bash'.format(user), st.st_mode | stat.S_IEXEC)
+    print 'Wrote environment script to "/home/{}/.ros/env/distributed_ros.bash".'.format(user)
+
+    print 'Finished writing files. Done.'
+
+else:
+    print 'Done. Nothing written to disk.'
